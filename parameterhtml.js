db28105dedc23b086900fda4bda369e8324d58fa
George Fawcett <georgealanfawcett@gmail.com>
2017-03-13 19:51:44 +0000

    Adds file was parameters

---
+++ parameterhtml.js
@@ -0,0 +1,39 @@
+function getAndPrintHTML () {
+
+  var https = require('https');
+
+  /* Add your code here */
+  var dataArray = [];
+
+  https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    dataArray.push(data);
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+      for (i = 0; i < dataArray.length; i++) {
+    console.log(`${dataArray[i]}\n`);
+  }
+  });
+
+
+});
+
+}
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+
+getAndPrintHTML();
